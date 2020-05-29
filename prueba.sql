59cf93c9df9d9ec0d3fdebf15c157b7e5247b9bd
Segundo Galdames <segundogaldames@gmail.com>
2020-05-29 16:00:02 -0400

    respaldo base

--- prueba.sql
+++ prueba.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.26, for osx10.9 (x86_64)
 --
 -- Host: localhost    Database: prueba
 -- ------------------------------------------------------
--- Server version	5.7.30-0ubuntu0.18.04.1
+-- Server version	5.7.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -72,6 +72,62 @@ LOCK TABLES `productos` WRITE;
 INSERT INTO `productos` VALUES (3,'Polera','P003-K',2590,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(4,'Camiseta deportiva','CD983-1',9890,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(6,'Chaqueta de cuero','ch0002-2',25990,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(7,'Sweeter','P003-1',12500,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(8,'Calceta nylon','P003-3',3500,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(9,'Chaleco lana','P003-2',349990,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(10,'Chaqueta de cuero','P003-4',35000,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(11,'Camisa de vestir','CV009-2',12990,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(12,'Pantalon de cuero','PC5678',9990,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(13,'Jeans','J8000',0,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(14,'Pantalon dama','PD789',9,1,'2020-05-22 20:58:00','2020-05-22 20:58:00'),(15,'Polera manga larga','PML001',9990,1,'2020-05-16 13:50:39','2020-05-16 13:50:39'),(16,'Camisa manga corta','CC0001',12990,2,'2020-05-22 21:54:25','2020-05-22 21:54:25');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Administrador','2020-05-29 13:05:04','2020-05-29 13:26:34'),(2,'Supervisor(a)','2020-05-29 14:49:54','2020-05-29 14:51:44'),(3,'Vendedor','2020-05-29 14:50:03','2020-05-29 14:57:37');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `activo` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Segundo Galdames','segundogaldames@gmail.com','7c222fb2927d828af22f592134e8932480637c0d',1,1,'2020-05-29 15:49:26','2020-05-29 15:49:26');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -82,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-29 12:26:56
+-- Dump completed on 2020-05-29 15:59:18
